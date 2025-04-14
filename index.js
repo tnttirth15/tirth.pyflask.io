5b1e0a9f2a99ecee60bf904fc615b48083843ea7
maverick-15 <122634425+maverick-15@users.noreply.github.com>
2025-04-14 01:09:51 -0400

    Bugs fixed
    I noticed that in the portfolio, couple of things were not appropriate. I saw that by inspecting in phone device that, the navbar was not appropriate and the content was overflowing in the web page, to fix that I used javascript and made a Hamburger menu. In addition, I noticed that there were some formatting errors in the code so I used the W3C for my css and HTML to fix it. I guess it gonna be this much for today.

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.getElementById("mobileMenuButton");
+  const mobileMenu = document.getElementById("mobileMenu");
 
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
+  menuButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+
+    // Change icon based on menu state
+    const icon = menuButton.querySelector("i");
+    if (mobileMenu.classList.contains("active")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-times");
+    } else {
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
+    }
+  });
+
+  // Close menu when clicking on a link
+  const mobileLinks = mobileMenu.querySelectorAll("a");
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      mobileMenu.classList.remove("active");
+      const icon = menuButton.querySelector("i");
+      icon.classList.remove("fa-times");
+      icon.classList.add("fa-bars");
     });
   });
+});
+
+// This will allow smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
